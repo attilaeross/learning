3321c21524f25ad5ec395b9a5e95a8811135a963
Attila Eross <attila.eross@yahoo.co.uk>
2020-12-16 14:06:47 +0000

    reformated app.js

--- TodoApp/app.js
+++ TodoApp/app.js
@@ -1,15 +1,17 @@
 // Getting Elements - selectors
 
-const addButton = document.querySelector('button.add');
-const todoList = document.querySelector('ul.list');
-const filterOption = document.querySelector('select.filter');
-const changeUserButton = document.querySelector('button.change-user');
-const listHeader = document.querySelector('h2.list-header');
+const addButton = document.querySelector("button.add");
+const todoList = document.querySelector("ul.list");
+const filterOption = document.querySelector("select.filter");
+const changeUserButton = document.querySelector("button.change-user");
+const listHeader = document.querySelector("h2.list-header");
 
 // Functions
 let userKey;
 const setUser = () => {
-  const userName = prompt('Please enter your name...Single name please...for now');
+  const userName = prompt(
+    "Please enter your name...Single name please...for now"
+  );
   if (!userName) {
     setUser();
   } else {
@@ -21,29 +23,29 @@ const setUser = () => {
 
 const addToList = (todo) => {
   // prepare the structure
-  const newTodo = document.createElement('li');
-  newTodo.classList.add('todo-item');
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
 
   // Add Todo text element
-  const textElement = document.createElement('p');
-  textElement.classList.add('todo-text');
+  const textElement = document.createElement("p");
+  textElement.classList.add("todo-text");
   textElement.innerHTML = todo.text;
   newTodo.appendChild(textElement);
 
   // check mark button
-  const completedButton = document.createElement('button');
-  completedButton.innerHTML = 'Mark';
-  completedButton.classList.add('complete-button');
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "Mark";
+  completedButton.classList.add("complete-button");
 
   if (todo.isComplete == true) {
-    newTodo.classList.toggle('complete');
+    newTodo.classList.toggle("complete");
   }
   newTodo.appendChild(completedButton);
 
   // edit button
-  const editButton = document.createElement('button');
-  editButton.innerHTML = 'Edit';
-  editButton.classList.add('edit-button');
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "Edit";
+  editButton.classList.add("edit-button");
 
   if (todo.isComplete == true) {
     editButton.disabled = true;
@@ -51,9 +53,9 @@ const addToList = (todo) => {
   newTodo.appendChild(editButton);
 
   // delete button
-  const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = 'Delete';
-  deleteButton.classList.add('delete-button');
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.classList.add("delete-button");
   newTodo.appendChild(deleteButton);
 
   // append to the list
@@ -121,18 +123,18 @@ const updateTodoTextLocalStorage = (oldText, newText) => {
 const getStoredTodos = () => {
   const storageKey = `${userKey}Todos`;
   if (localStorage.getItem(storageKey) === null) {
-    return todos = [];
+    return (todos = []);
   }
-  return todos = JSON.parse(localStorage.getItem(storageKey));
+  return (todos = JSON.parse(localStorage.getItem(storageKey)));
 };
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', setUser);
+document.addEventListener("DOMContentLoaded", setUser);
 
-addButton.addEventListener('click', (event) => {
+addButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const textInput = document.querySelector('input.new-todo');
+  const textInput = document.querySelector("input.new-todo");
   const todo = {
     text: textInput.value,
     isComplete: false,
@@ -140,20 +142,20 @@ addButton.addEventListener('click', (event) => {
   addToList(todo);
   addToLocalStorage(todo);
   // clear todo input value;
-  textInput.value = '';
+  textInput.value = "";
 });
 
 let oldTodoText;
-todoList.addEventListener('click', (event) => {
+todoList.addEventListener("click", (event) => {
   const item = event.target;
   const todo = item.parentElement;
 
   // set/mark Todo DONE / UNDONE
-  if (item.classList[0] === 'complete-button') {
-    todo.classList.toggle('complete');
+  if (item.classList[0] === "complete-button") {
+    todo.classList.toggle("complete");
     editButton = todo.childNodes[2];
 
-    if (todo.classList.contains('complete')) {
+    if (todo.classList.contains("complete")) {
       todo.isComplete = true;
       editButton.disabled = true;
       changeTodoMarkLocalStorage(todo);
@@ -165,28 +167,28 @@ todoList.addEventListener('click', (event) => {
   }
 
   // Edit todo description
-  if (item.classList[0] === 'edit-button') {
+  if (item.classList[0] === "edit-button") {
     const todoElement = todo.childNodes[0];
-    if (item.innerHTML == 'Edit') {
+    if (item.innerHTML == "Edit") {
       oldTodoText = todoElement.innerText;
       todoElement.contentEditable = true;
       todoElement.focus();
-      item.innerHTML = 'Save';
+      item.innerHTML = "Save";
 
       // disable all buttons (except Save) until user saves the todo text
-      const buttons = document.getElementsByTagName('button');
+      const buttons = document.getElementsByTagName("button");
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].innerHTML != 'Save') {
+        if (buttons[i].innerHTML != "Save") {
           buttons[i].disabled = true;
         }
       }
     } else {
       todoElement.contentEditable = false;
       const newTodoText = todoElement.innerText;
-      item.innerHTML = 'Edit';
+      item.innerHTML = "Edit";
 
       // enable all buttons after user saved the todo text
-      const buttons = document.getElementsByTagName('button');
+      const buttons = document.getElementsByTagName("button");
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = false;
       }
@@ -195,35 +197,35 @@ todoList.addEventListener('click', (event) => {
   }
 
   // Delete Todo
-  if (item.classList[0] === 'delete-button') {
+  if (item.classList[0] === "delete-button") {
     removeStoredTodo(todo);
     todo.remove();
   }
 });
 
-changeUserButton.addEventListener('click', setUser);
+changeUserButton.addEventListener("click", setUser);
 
-filterOption.addEventListener('change', (event) => {
+filterOption.addEventListener("change", (event) => {
   const todos = todoList.childNodes;
   todos.forEach((todo) => {
     switch (event.target.value) {
-    case 'all':
-      todo.style.display = 'flex';
-      break;
-    case 'completed':
-      if (todo.classList.contains('complete')) {
-        todo.style.display = 'flex';
-      } else {
-        todo.style.display = 'none';
-      }
-      break;
-    case 'outstanding':
-      if (!todo.classList.contains('complete')) {
-        todo.style.display = 'flex';
-      } else {
-        todo.style.display = 'none';
-      }
-      break;
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("complete")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "outstanding":
+        if (!todo.classList.contains("complete")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
     }
   });
 });
